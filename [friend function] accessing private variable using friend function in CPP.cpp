652afd2dd4e5263348df0f56f1ab8e0d425d89c8
arpudhanaresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-03 09:17:05 +0530

    Update [friend function] accessing private variable using friend function in CPP.cpp

--- [friend function] accessing private variable using friend function in CPP.cpp
+++ [friend function] accessing private variable using friend function in CPP.cpp
@@ -1,31 +1,24 @@
 #include<iostream>
 using namespace std;
-int arpudha (int a , int b)
+
+class arpudha
 {
-    int c;
-    c=a+b;
-    return c;
-}
-double arpudha (double a , double b)
+private:
+        int a=10;
+public:
+    addi();
+    friend int display(arpudha);
+};
+int display(arpudha display)
 {
-    double c;
-    c=a/b;
-    return c;
-}
-char arpudha()
-{
-    char a= 'a';
-    return a;
-}
+        display.a;
+        return display.a;
+    }
 int main()
 {
-    int integer;
-    double double1;
-    char character;
-    integer=arpudha(2,3);
-    double1=arpudha(2.1,4.3);
-    cout<<"This is integer: "<<integer<<endl;
-    cout<<"This is double: "<<double1<<endl;
-    cout<<"This is a character: "<<arpudha();
+    arpudha A;
+
+
+    cout<<"private varaible a :"<<display(A);
 }
 
