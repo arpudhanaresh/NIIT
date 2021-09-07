55db2fb0478be3fbf3aa99e288ddc2e62b4f8405
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-07 12:01:08 +0530

    Create array pointer.cpp

---
+++ array pointer.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10], *ptr[10],i;
+    cout<<"Enter 5 values:"<<endl;
+    for(i=0;i<=4;i++)
+    {
+        cin>>a[i];
+    }
+    for(i=0;i<=4;i++)
+    {
+        ptr[i]=&a[i];
+        cout<<ptr[i]<<endl;;
+    }
+
+}
