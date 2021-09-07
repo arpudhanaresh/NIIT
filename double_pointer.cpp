c26609c048dd5a624489dd880cb1fc62ff4c0c6e
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-07 20:11:49 +0530

    Create double_pointer.cpp

---
+++ double_pointer.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int *ptr , **dptr , a ;
+    a=50;
+    ptr=&a;
+    dptr=&ptr;
+    cout<<"Value of a: "<<a<<endl;
+    cout<<"Address of a :"<<&a<<endl;
+    cout<<"address of a stored in pointer: "<<ptr<<endl;
+    cout<<"value of  address stored in pointer: "<<*ptr<<endl;
+    cout<<"address stored in double pointer: "<<dptr<<endl;
+    cout<<"value of address poiting the pointer: "<<*dptr<<endl;
+    cout<<"value stored in pointing the another pointer: "<<**dptr<<endl;
+}
