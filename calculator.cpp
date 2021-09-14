84a15d2e78ae7973a671b931a55833b502e672c7
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-14 10:05:00 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+using namespace std;
+class calc
+{
+public:
+    calc()
+    {
+        int a=0, b=0, c=0;
+    }
+    int add(int a, int b)
+    {
+
+       int c=a+b;
+        return c;
+    }
+    int sub(int a, int b)
+    {
+        int c=a-b;
+        return c;
+    }
+     int mul(int a, int b)
+    {
+        int c=a*b;
+        return c;
+    }
+     int div (int a, int b)
+    {
+        int c=a/b;
+        return c;
+    }
+};
+
+int main()
+{
+    calc cal;
+    int a,b,n;
+    cout<<"Enter number 1:";
+    cin>>a;
+    cout<<"Enter the number 2:";
+    cin>>b;
+    cout<<"Enter 1 for Addition"<<endl;
+    cout<<"Enter 2 for subtraction"<<endl;
+    cout<<"Enter 3 for multiplication"<<endl;
+    cout<<"Enter 4 for Division"<<endl;
+    cin>>n;
+    switch(n)
+    {
+    case 1:
+        {
+            cout<<"The addition of number 1 and number 2:"<<cal.add(a,b)<<endl;
+            break;
+        }
+    case 2:
+         {
+            cout<<"The subtraction of number 1 and number 2:"<<cal.sub(a,b)<<endl;
+            break;
+        }
+    case 3:
+         {
+            cout<<"The multiplication of number 1 and number 2:"<<cal.mul(a,b)<<endl;
+            break;
+        }
+    case 4:
+         {
+            cout<<"The division  of number 1 and number 2:"<<cal.div(a,b)<<endl;
+            break;
+        }
+    default:
+        {
+            cout<<"Invalid Entry";
+        }
+    }
+}
