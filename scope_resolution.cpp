4015aeee8d940c483a82431b42ef88c33dc9cbe3
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-16 11:38:54 +0530

    Update scope_resolution.cpp

--- scope_resolution.cpp
+++ scope_resolution.cpp
@@ -3,12 +3,18 @@ using namespace std;
 class arpudha
 {
 public:
+    arpudha();
     void test();
 };
 void arpudha::test()
 {
     cout<<"test1"<<endl;
 }
+arpudha::arpudha()
+{
+    cout<<"This is constructor"<<endl;
+}
+
 int main()
 {
     arpudha a;
