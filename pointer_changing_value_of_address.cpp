2951bdc80e689ebc5542cbef7af5e4d8a23662d2
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-16 12:17:21 +0530

    Update pointer_changing_value_of_address.cpp

--- pointer_changing_value_of_address.cpp
+++ pointer_changing_value_of_address.cpp
@@ -5,7 +5,7 @@ int main()
     int a=50;
     int *c;
     cout<<"value of a:"<<a<<endl;
-    cout<<"address of c: "c<<endl;
+    cout<<"address of c: "c<<endl;  //garbage value
     cout<<"address of a:"<<&a<<endl;
     c=&a;
     *c=30;
