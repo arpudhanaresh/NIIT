1c88116922e8d6806f4f9afaebfb7a89a0f08049
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-16 12:01:37 +0530

    Create pointer_changing_value_of_address.cpp

---
+++ pointer_changing_value_of_address.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=50;
+    int *c;
+    cout<<"value of a:"<<a<<endl;
+    cout<<"address of a:"<<&a<<endl;
+    c=&a;
+    *c=30;
+    cout<<"value of a:"<<a<<endl;
+}
