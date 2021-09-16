3131cc903fe33c47e266a32913b6f0528ddd1263
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-16 10:36:59 +0530

    Create class_inheritance_all.cpp

---
+++ class_inheritance_all.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class arpudha
+
+{
+    public:
+    arpudha()
+    {
+        cout<<"Arpudha Constructor is called"<<endl;
+    }
+
+
+};
+class arp1 : public arpudha
+{
+    public:
+    arp1()
+    {
+        cout<<"arp1 contructor without overloading"<<endl;
+    }
+    arp1(int a , int b)
+    {
+        cout<<"inherited constructor overloading"<<endl;
+        cout<<"arp1 constructor is called"<<endl;
+        cout<<"value of a:"<<a<<"\n"<<"value of b:"<<b<<endl;
+    }
+    int func1()
+    {
+        cout<<"func1 inheritance function called"<<endl;
+    }
+
+};
+int main()
+{
+    arp1 a(12,4);
+//    a(9,4);
+    arpudha c;
+//    a.funcl();
+}
