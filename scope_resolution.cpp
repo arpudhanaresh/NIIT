235c8e01852b2e3b3c182e84f9d79e2811e21d28
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-16 11:37:07 +0530

    Create scope_resolution.cpp

---
+++ scope_resolution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class arpudha
+{
+public:
+    void test();
+};
+void arpudha::test()
+{
+    cout<<"test1"<<endl;
+}
+int main()
+{
+    arpudha a;
+    a.test();
+    return 0;
+}
