b3ecbc702624278eb02defb7956edac8316b99f6
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-17 11:08:36 +0530

    Create swapusingclass.cpp

---
+++ swapusingclass.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class arpudha
+{
+public:
+    void swap(int a,int b)
+{
+    int temp;
+    temp =a;
+    a=b;
+    b=temp;
+    cout<<"Value of a After swapped: "<<a<<endl;
+    cout<<"Value of b after swapped: "<<b<<endl;
+
+
+}
+};
+int main()
+{
+    int a=10,b=20;
+    cout<<"Value of a Before swap: "<<a<<endl;
+    cout<<"Value of b before swap: "<<b<<endl;
+    arpudha obj;
+    obj.swap(a,b);
+    cout<<"Value of a after swap function : "<<a<<endl;
+    cout<<"Value of b after swap function: "<<b<<endl;
+    return 0;
+}
+
