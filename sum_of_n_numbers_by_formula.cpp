4cf62df96a961c2251ec9870761584fd9d05c812
Arpudha Naresh <59797552+arpudhanaresh@users.noreply.github.com>
2021-09-28 09:13:01 +0530

    Add files via upload

---
+++ sum_of_n_numbers_by_formula.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,sum=0;
+    cout<<"Enter value of n:";
+    cin>>n;
+    sum= (n*(n+1))/2;
+    cout<<"The sum of n numbers is :"<<sum;
+    return 0;
+}
